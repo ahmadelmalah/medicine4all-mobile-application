1e57e25a42485377e0862a1f99ce0bf217a8cb78
Ahmad <ahmedelmalah@hotmail.com>
2017-01-11 18:06:27 +0200

    Preparing angular app for using services

---
+++ www/js/services.js
@@ -0,0 +1,13 @@
+angular.module('starter.services', ['ngResource'])
+ .constant("apiURL","http://nichepharma.com/medicine4all-backend/public/api/")
+
+// .factory('Announcement', ['$resource', 'apiURL', function ($resource, apiURL) {
+//   return $resource(apiURL + "announcements/:id", null, {
+//       // 'update': {
+//       //     method: 'PUT'
+//       // }
+//   });
+// }])
+
+
+;
